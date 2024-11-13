e8e80c24f2f07642f444e539ecbfc064d29b614f
HaiderAli228 <flutter2830@gmail.com>
2024-11-13 19:51:12 +0500

    adding the Navigation bar in app

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 class AppColors{
   static const Color themeColor = Colors.blue ;
+  static const Color themeIconColor = Colors.white ;
   static const Color themeTextColor = Colors.white ;
 }
--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,24 +1,60 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:docsview/utils/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import '../utils/app_colors.dart';
-
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => HomeViewState();
+}
+
+class HomeViewState extends State<HomeView> {
+  int _page = 0;
+  final GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: AppColors.themeColor,
-          title: const Text(
-            " ",
-            style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                fontFamily: "Poppins"),
+      backgroundColor: Colors.white,
+      body: Container(
+        // color: Colors.blueAccent,
+        child: Center(
+          child: Column(
+            children: [
+              Text(_page.toString(), textScaleFactor: 10.0),
+              ElevatedButton(
+                child: const Text('Go To Page of index 1'),
+                onPressed: () {
+                  final CurvedNavigationBarState? navBarState =
+                      _bottomNavigationKey.currentState;
+                  navBarState?.setPage(1);
+                },
+              )
+            ],
           ),
         ),
-        body: const Column());
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.white,
+        color: AppColors.themeColor,
+        key: _bottomNavigationKey,
+        animationCurve: Curves.linear,
+        items: const [
+          Icon(FontAwesomeIcons.houseChimneyUser,
+              color: AppColors.themeIconColor, size: 30),
+          Icon(FontAwesomeIcons.solidClipboard,
+              color: AppColors.themeIconColor, size: 30),
+          Icon(FontAwesomeIcons.solidCircleUser,
+              color: AppColors.themeIconColor, size: 30),
+        ],
+        onTap: (index) {
+          setState(() {
+            _page = index;
+          });
+        },
+      ),
+    );
   }
 }
