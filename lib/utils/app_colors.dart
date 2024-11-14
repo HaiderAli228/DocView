3c26abdc2c8028a2f15d9110f63849fd24f7e947
HaiderAli228 <flutter2830@gmail.com>
2024-11-14 15:35:56 +0500

    adding intro and animation

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,4 +3,5 @@ class AppColors{
   static const Color themeColor = Colors.blue ;
   static const Color themeIconColor = Colors.white ;
   static const Color themeTextColor = Colors.white ;
+  static const Color backgroundBodyColor = Colors.white ;
 }
