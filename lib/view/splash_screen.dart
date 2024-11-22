5c99f0a05987db065142ec8116f5b7fe3201976a
HaiderAli228 <flutter2830@gmail.com>
2024-11-22 08:51:43 +0500

    adding splash screen

---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,25 @@
+import 'package:docsview/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.themeColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
